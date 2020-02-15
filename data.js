a2cf599a164f5e746366b004f60ed72dbcc05a18
akups <appiah-lindberg@outlook.com>
2020-02-15 16:02:09 +0100

    data

--- data.js
+++ data.js
@@ -1,39 +1,39 @@
-// const listings = [
-//   {
-//     id: "1",
-//     header:
-//       "Furnished, Unique, Ethnic Style Apt- For short or long period!!!!! 400",
-//     price: 400,
-//     number_of_bedrooms: null,
-//     size: null,
-//     location: "berlin",
-//     picture_url: ""
-//   },
-//   {
-//     id: "2",
-//     header: " TOP FLOOR APARTMENT with terrace - MARCH 2020",
-//     price: 1000,
-//     number_of_bedrooms: 1,
-//     size: 70,
-//     location: "Berlin, Mitte",
-//     picture_url: "https://images.craigslist.org/00202_cVdQnUehbdv_600x450.jpg"
-//   },
-//   {
-//     id: "3",
-//     header: " 43mq new and cosy Flat in Rummelsburg / Lichtenberg Berlin",
-//     price: 850,
-//     number_of_bedrooms: 2,
-//     size: null,
-//     location: "Berlin",
-//     picture_url: "https://images.craigslist.org/00808_2WFMCjjqS5F_600x450.jpg"
-//   }
-// ];
+const listings = [
+  {
+    id: "1",
+    header:
+      "Furnished, Unique, Ethnic Style Apt- For short or long period!!!!! 400",
+    price: 400,
+    number_of_bedrooms: null,
+    size: null,
+    location: "berlin",
+    picture_url: ""
+  },
+  {
+    id: "2",
+    header: " TOP FLOOR APARTMENT with terrace - MARCH 2020",
+    price: 1000,
+    number_of_bedrooms: 1,
+    size: 70,
+    location: "Berlin, Mitte",
+    picture_url: "https://images.craigslist.org/00202_cVdQnUehbdv_600x450.jpg"
+  },
+  {
+    id: "3",
+    header: " 43mq new and cosy Flat in Rummelsburg / Lichtenberg Berlin",
+    price: 850,
+    number_of_bedrooms: 2,
+    size: null,
+    location: "Berlin",
+    picture_url: "https://images.craigslist.org/00808_2WFMCjjqS5F_600x450.jpg"
+  }
+];
 
 
-function dynamicCardRendering(arr) {
+function dynamicCardRendering(listings) {
   const cardsWrapper = document.querySelector("cards-wrapper");
 
-  arr.forEach(el => {
+  listings.forEach(el => {
     document.querySelector(".cards-wrapper").innerHTML = "";
 
     const newCard = `<div class="card col-3">
