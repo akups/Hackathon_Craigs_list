5f7110b0ee9bbbcf2e42a52e889b73b0dfe04fab
Jacqueline Chen <xjacquelinechen@gmail.com>
2020-02-15 13:50:34 +0100

    Deleted irongenerate structure and added new files

---
+++ data.js
@@ -0,0 +1,29 @@
+const listings = [
+  {
+    id: "1",
+    header: "Furnished, Unique, Ethnic Style Apt- For short or long period!!!!! 400",
+    price: 400,
+    number_of_bedrooms: null,
+    size: null,
+    location: "berlin",
+    picture_url: ""
+  },
+  {
+    id: "2",
+    header: " TOP FLOOR APARTMENT with terrace - MARCH 2020",
+    price: 1000,
+    number_of_bedrooms: 1,
+    size: 70,
+    location: "Berlin, Mitte",
+    picture_url: "https://images.craigslist.org/00202_cVdQnUehbdv_600x450.jpg"
+  },
+  {
+    id: "3",
+    header: " 43mq new and cosy Flat in Rummelsburg / Lichtenberg Berlin",
+    price: 850,
+    number_of_bedrooms: 2,
+    size: null,
+    location: "Berlin",
+    picture_url: "https://images.craigslist.org/00808_2WFMCjjqS5F_600x450.jpg"
+  }
+];
